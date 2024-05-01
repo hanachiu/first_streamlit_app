e59daa75a65554bba6536b36bef8985309383c63
hanachiu <chiu.hana@gmail.com>
2024-05-02 07:45:57 +0800

    Update streamilt_app.py

--- streamilt_app.py
+++ streamilt_app.py
@@ -1,66 +1,35 @@
-import streamlit
-import pandas
-import requests
-import snowflake 
-import snowflake.connector
-from urllib.error import URLError
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+import pandas as pd
 
-streamlit.title('My parents New Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-# Let's put a pick list here so they can pick the fruit they want to include 
-fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
- #fruits_to_show = streamlit.dataframe(my_fruit_list[my_fruit_list["app"].isin(options)])
-# Display the table on the page.
-streamlit.dataframe(fruits_to_show)
+# Write directly to the app
+st.title("Zena's Amazing Athleisure Catalog")
+st.write(
+    """Pick a sweatsuit color or style:
+    """
+)
 
-streamlit.header("Fruityvice Fruit Advice!") 
-def get_fruityvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+this_fruit_choice)
-    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return fruityvice_normalized
-try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-        streamlit.error("Please select a fruit to get information.")
-    else:
-        back_from_function = request.get("https://fruityvice.com/api/fruit/" + fruit_choice) 
-        streamlit.dataframe(back_from_function)
-except URLError as e:
-    streamlit.error()
+# Get the current credentials
+session = get_active_session()
+my_catlog=session.table('catalog_for_website').select(col('COLOR_OR_STYLE'))
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json())
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
-def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("select * from fruit_load_list")
-        return my_cur.fetchall()
-if streamlit.button('Get Fruit Load List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    my_data_rows = get_fruit_load_list()
-    my_cnx.close()
-    streamlit.dataframe(my_data_rows)
-    
-def insert_row_snowflake(new_fruit):
-    with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('" + new_fruit + "')")
-        return "Thanks for adding "+ new_fruit
+option=st.selectbox('enter',my_catlog)
 
-add_my_fruit = streamlit.text_input("What fruit would you like to add?")
-if streamlit.button('Add a Fruit to the List'):
-    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-    back_from_function = insert_row_snowflake(add_my_fruit)
-    my_cnx.close()
- 
+# We'll build the image caption now, since we can
+product_caption = 'Our warm, comfortable, ' + option + ' sweatsuit!'
+
+ans=session.table('catalog_for_website').filter(col('COLOR_OR_STYLE')==option)
+#st.write(ans)
+p_df=ans.to_pandas()
+#st.write(p_df)
+#df2 = session.fetchone()
+
+
+#st.write(p_df['DIRECT_URL'].iloc[0])
+st.image(p_df['DIRECT_URL'].iloc[0],width=400,caption=product_caption)
+
+st.write('Price:',p_df['PRICE'].iloc[0])
+st.write('Sizes Available::',p_df['SIZE_LIST'].iloc[0])
+st.write(p_df['UPSELL_PRODUCT_DESC'].iloc[0])
